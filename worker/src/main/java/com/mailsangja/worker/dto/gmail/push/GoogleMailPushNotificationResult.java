365a37cba66792af4edfc8a5193b43b2a269a69b
휘래 김 <hramst0618@gmail.com>
2026-04-21 16:14:00 +0900

    feat: push/sync/watch dto 계약 갱신 (#54)

--- worker/src/main/java/com/mailsangja/worker/dto/gmail/push/GoogleMailPushNotificationResult.java
+++ worker/src/main/java/com/mailsangja/worker/dto/gmail/push/GoogleMailPushNotificationResult.java
@@ -1,7 +1,18 @@
 package com.mailsangja.worker.dto.gmail.push;
 
+import com.mailsangja.worker.common.exception.mail.MailPushErrorCode;
+import com.mailsangja.worker.common.exception.mail.MailPushException;
+
 public record GoogleMailPushNotificationResult(
         String emailAddress,
         String historyId
 ) {
+    public GoogleMailPushNotificationResult {
+        if (emailAddress == null || emailAddress.isBlank()) {
+            throw new MailPushException(MailPushErrorCode.INVALID_GMAIL_PUSH_NOTIFICATION);
+        }
+        if (historyId == null || historyId.isBlank()) {
+            throw new MailPushException(MailPushErrorCode.INVALID_GMAIL_PUSH_NOTIFICATION);
+        }
+    }
 }
