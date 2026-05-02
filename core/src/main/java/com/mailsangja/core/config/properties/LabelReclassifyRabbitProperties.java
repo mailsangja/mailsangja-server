d704d57093a897c83a80666aa74f84ad476a45b0
휘래 김 <hramst0618@gmail.com>
2026-05-02 17:17:11 +0900

    feat: 라벨 재분류를 위한 RabbitMQ 배치 설정 추가 (#81)

--- core/src/main/java/com/mailsangja/core/config/properties/LabelReclassifyRabbitProperties.java
+++ core/src/main/java/com/mailsangja/core/config/properties/LabelReclassifyRabbitProperties.java
@@ -1,11 +1,19 @@
 package com.mailsangja.core.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
+@ConfigurationProperties(prefix = "mailsangja.rabbitmq.label-reclassify")
 public class LabelReclassifyRabbitProperties {
 
     private static final String TASK_NAME = "label.reclassify";
+
+    private int threadBatchSize = 50;
 
     public String getTaskName() {
         return TASK_NAME;
--- worker/src/main/java/com/mailsangja/worker/config/properties/LabelReclassifyRabbitProperties.java
+++ worker/src/main/java/com/mailsangja/worker/config/properties/LabelReclassifyRabbitProperties.java
@@ -1,11 +1,20 @@
 package com.mailsangja.worker.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
+@ConfigurationProperties(prefix = "mailsangja.rabbitmq.label-reclassify")
 public class LabelReclassifyRabbitProperties {
 
     private static final String TASK_NAME = "label.reclassify";
+
+    private int messageBatchSize = 200;
+    private int threadBatchSize = 50;
 
     public String getTaskName() {
         return TASK_NAME;
