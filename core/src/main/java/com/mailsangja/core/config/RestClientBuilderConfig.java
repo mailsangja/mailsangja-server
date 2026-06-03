c7b244a710e1ac49bf6dffb590fbbffaa30a0252
천 진강 <jjjjjk12@ajou.ac.kr>
2026-06-03 22:04:44 +0900

    chore: Grafana 대시보드 세팅 (#155)

---
+++ core/src/main/java/com/mailsangja/core/config/RestClientBuilderConfig.java
@@ -0,0 +1,20 @@
+package com.mailsangja.core.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientBuilderConfig {
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RestClient.Builder restClientBuilder(ObjectProvider<ObservationRegistry> observationRegistryProvider) {
+        return RestClient.builder()
+                .observationRegistry(observationRegistryProvider.getIfAvailable(() -> ObservationRegistry.NOOP));
+    }
+}
